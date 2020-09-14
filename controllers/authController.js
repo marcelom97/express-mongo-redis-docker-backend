d08a53b450e74cda1636a096199d4d164eb2f7df
marcelom97 <marcelomollaj@gmail.com>
2020-09-14 17:56:33 +0300

    [added] send cookie to chrome

--- controllers/authController.js
+++ controllers/authController.js
@@ -199,7 +199,8 @@ const sendTokenResponse = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true
+    httpOnly: true,
+    secure: false
   };
 
   if (process.env.NODE_ENV === 'production') {
--- server.js
+++ server.js
@@ -4,9 +4,15 @@ const connectDB = require('./config/connectDB');
 const { json } = require('body-parser');
 const errorHandler = require('./middlewares/errorHandler');
 const morgan = require('morgan');
-const { yellow, bold, blue } = require('colors');
+const { yellow, bold, red } = require('colors');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xXssProtection = require('x-xss-protection');
+const hpp = require('hpp');
+const cookieSession = require('cookie-session');
+
 // Adding environment variables
 dotenv.config({ path: './config/config.env' });
 
@@ -18,17 +24,31 @@ const roomsRouter = require('./routes/roomsRouter');
 const servicesRouter = require('./routes/servicesRouter');
 
 const app = express();
+app.set('trust proxy', true);
+
 connectDB();
 
 // Mount middlewares
 // Needed to be able to read body data
 app.use(json());
+app.use(express.urlencoded({ extended: true }));
 // Mount custome logger to Server
 app.use(morgan('dev'));
 // use express middleware for easier cookie handling
 app.use(cookieParser());
+
 // use cors
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    credentials: true
+  })
+);
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(xXssProtection());
+app.use(hpp());
 
 // Mount routes
 app.use('/api/v1/message', messageRouter);
@@ -43,4 +63,9 @@ app.use(errorHandler);
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(yellow(bold(`Listening on http://localhost:${port}`)));
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(red(`Error: ${err.message}`));
 });
